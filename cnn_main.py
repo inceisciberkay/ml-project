40beb7811c5fedc21dadec57f2085c3e0f625a28
berkay.inceisci <berkay.inceisci@esensi.com.tr>
2023-12-11 17:24:25 +0300

    WIP

--- cnn_main.py
+++ cnn_main.py
@@ -6,6 +6,8 @@ from keras.applications.xception import Xception
 from keras.layers import GlobalAveragePooling2D, Dense, Dropout, Conv2D, MaxPooling2D
 from keras.optimizers import Adam
 from keras.callbacks import EarlyStopping, ReduceLROnPlateau
+from keras.losses import BinaryCrossentropy
+from keras.metrics import BinaryAccuracy
 
 from dataset_preparer import get_dataset, IMG_SIZE
 
@@ -19,41 +21,42 @@ train_ds = get_dataset('train')
 validation_ds = get_dataset('validation')
 test_ds = get_dataset('test')
 
-# Create the base model from the pre-trained model MobileNet V2
-base_model = Xception(input_shape=IMG_SHAPE, include_top=False, weights='imagenet', pooling='avg')
+# fine_tune_at = len(base_model.layers) - 100
+# # Freeze the pre-trained layers
+# for layer in base_model.layers[:fine_tune_at]: # try
+#     layer.trainable = False
+
+# # Remove last layers
+# x = base_model.layers[fine_tune_at].output # try
+# x = Conv2D(16, (3, 3), activation='relu', padding='same')(x) #try
+# x = MaxPooling2D()(x)
+# x = Conv2D(32, (3, 3), activation='relu', padding='same')(x) #try
+# x = MaxPooling2D()(x)
+# x = Conv2D(64, (3, 3), activation='relu', padding='same')(x) #try
+# x = MaxPooling2D()(x)
+
+# # help me to add flatten laye
+# x = GlobalAveragePooling2D()(x)
+# x = Dense(256, activation='relu')(x)
+# x = Dropout(0.2)(x)  # Add dropout with a rate of 0.5 (adjust as needed)
+# x = Dense(128, activation='relu')(x)
+# x = Dense(16, activation='relu')(x)
+
+# Create the base model from the pre-trained model Xception
+base_model = Xception(input_shape=IMG_SHAPE, include_top=False, weights='imagenet')
+print(base_model.summary())
 
 # Freeze the base_model
-base_model.trainable = False # try
-fine_tune_at = len(base_model.layers) - 100
-# Freeze the pre-trained layers
-for layer in base_model.layers[:fine_tune_at]: # try
-    layer.trainable = False
+base_model.trainable = False
 
-# Remove last layers
-x = base_model.layers[fine_tune_at].output # try
-x = Conv2D(16, (3, 3), activation='relu', padding='same')(x) #try
-x = MaxPooling2D()(x)
-x = Conv2D(32, (3, 3), activation='relu', padding='same')(x) #try
-x = MaxPooling2D()(x)
-x = Conv2D(64, (3, 3), activation='relu', padding='same')(x) #try
-x = MaxPooling2D()(x)
-
-# help me to add flatten laye
+inputs = tf.keras.Input(shape=IMG_SHAPE)
+x = base_model(inputs, training=False)   # run base_model in inference mode
 x = GlobalAveragePooling2D()(x)
-x = Dense(256, activation='relu')(x)
-x = Dropout(0.2)(x)  # Add dropout with a rate of 0.5 (adjust as needed)
-x = Dense(128, activation='relu')(x)
-x = Dense(16, activation='relu')(x)
+outputs = Dense(1)(x)
 
-# x = BatchNormalization()(x)
-predictions = Dense(1, activation='sigmoid')(x)
+model = tf.keras.Model(inputs, outputs)
 
-# Create the model for fine-tuning
-model = tf.keras.Model(inputs=base_model.input, outputs=predictions)
-model.summary(show_trainable=True) # try
-
-# Compile the model
-model.compile(optimizer=Adam(learning_rate=0.0001), loss='binary_crossentropy', metrics=['accuracy'])
+model.compile(optimizer=Adam(), loss=BinaryCrossentropy(from_logits=True), metrics=[BinaryAccuracy()])
 
 # Display the model summary
 model.summary()
@@ -69,26 +72,46 @@ hist = model.fit(
     callbacks=[RemoveGarbageCallback(), tensorboard_callback, earlystop, reduce_lr]
 )
 
+# Fine Tuning
+
+# Unfreeze the last layers of base model
+count_unfreezed_layers = 100
+for layer in base_model.layers[-count_unfreezed_layers:]:
+    layer.trainable = True
+
+# recompiling the model
+model.compile(optimizer=Adam(1e-5),  # Very low learning rate
+              loss=BinaryCrossentropy(from_logits=True),
+              metrics=[BinaryAccuracy()])
+
+# Train fine-tuned model
+hist = model.fit(
+    train_ds,
+    validation_data=validation_ds,
+    epochs=20,
+    callbacks=[RemoveGarbageCallback(), tensorboard_callback, earlystop, reduce_lr]
+)
+
 test_loss, test_accuracy = model.evaluate(test_ds)
 print(f'Test Loss: {test_loss:.4f}')
 print(f'Test Accuracy: {test_accuracy * 100:.2f}%')
     # Plot training and validation loss
-plt.plot(hist.history['loss'], label='Training Loss')
-plt.plot(hist.history['val_loss'], label='Validation Loss')
-plt.title('Training and Validation Loss')
-plt.xlabel('Epoch')
-plt.ylabel('Loss')
-plt.legend()
-plt.show()
+# plt.plot(hist.history['loss'], label='Training Loss')
+# plt.plot(hist.history['val_loss'], label='Validation Loss')
+# plt.title('Training and Validation Loss')
+# plt.xlabel('Epoch')
+# plt.ylabel('Loss')
+# plt.legend()
+# plt.show()
 
-# Plot training and validation accuracy
-plt.plot(hist.history['accuracy'], label='Training Accuracy')
-plt.plot(hist.history['val_accuracy'], label='Validation Accuracy')
-plt.title('Training and Validation Accuracy')
-plt.xlabel('Epoch')
-plt.ylabel('Accuracy')
-plt.legend()
-plt.show()
+# # Plot training and validation accuracy
+# plt.plot(hist.history['accuracy'], label='Training Accuracy')
+# plt.plot(hist.history['val_accuracy'], label='Validation Accuracy')
+# plt.title('Training and Validation Accuracy')
+# plt.xlabel('Epoch')
+# plt.ylabel('Accuracy')
+# plt.legend()
+# plt.show()
 
 y_pred = model.predict(test_ds)
 # Convert probabilities to binary predictions (0 or 1)
